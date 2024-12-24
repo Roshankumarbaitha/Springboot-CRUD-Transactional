d45f620b7a0eac73a14c5b795f51a090d121acc1
Roshan kumar <RoshanKumarbaitha>
2024-12-24 17:31:32 +0530

    springboot-CRUD-transactional

---
+++ src/test/java/com/example/Springboot_CRUD_Transactional/SpringbootCrudTransactionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Springboot_CRUD_Transactional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudTransactionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
